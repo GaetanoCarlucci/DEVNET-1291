658d754c2a39636283e9a7d2fd13282ab865aa90
Gaetano Carlucci <gaetano.carlucci@gmail.com>
2021-12-16 17:36:36 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -141,10 +141,16 @@ module "terraform-intersight-iks" {
   organization = var.organization
   tags         = var.tags
 }
-  
-  
-#Wait for cluster to come up and then outpt the kubeconfig, if successful
-output "kube_config" {
-	value = data.intersight_kubernetes_cluster.kubeconfig.results[0].kube_config.kubeprofaction.kube_config[0].kube_config
-
+ 
+data "intersight_kubernetes_cluster" "kubeconfig" {
+  name = var.cluster_names["iksterraformk8scluster"]
 }
+	
+provider "kubernetes" {
+
+  host                   = yamldecode(base64decode(data.intersight_kubernetes_cluster.kubeconfig.results[0].kube_config)).clusters[0].cluster.server
+  cluster_ca_certificate = base64decode(yamldecode(base64decode(data.intersight_kubernetes_cluster.kubeconfig.results[0].kube_config)).clusters[0].cluster.certificate-authority-data)
+  client_certificate     = base64decode(yamldecode(base64decode(data.intersight_kubernetes_cluster.kubeconfig.results[0].kube_config)).users[0].user.client-certificate-data)
+  client_key             = base64decode(yamldecode(base64decode(data.intersight_kubernetes_cluster.kubeconfig.results[0].kube_config)).users[0].user.client-key-data)
+} 
+
