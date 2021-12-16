987dd90184b35bf2972513e1823593bf7a5e1216
Gaetano Carlucci <gaetano.carlucci@gmail.com>
2021-12-16 18:00:39 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -145,3 +145,4 @@ module "terraform-intersight-iks" {
 #Wait for cluster to come up and then outpt the kubeconfig, if successful
 output "kube_config" {
 	value = module.terraform-intersight-iks.module.cluster_profile.intersight_kubernetes_cluster_profile.kubeprofaction.kube_config[0].kube_config
+}
